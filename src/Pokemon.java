bcc8bbb82a44723bd5e0c6830dcace2f9b6a0cd6
pantaj <javier_gonor@hotmail.com>
2015-05-08 13:21:20 +0200

    POKEDEX- Javier González

---
+++ src/Pokemon.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xp
+ */
+public class Pokemon {
+    public String nombre = "";
+    public int generation_id = 0;
+    public int evolution_chain_id = 0;
+    public String species ="";
+    public String color ="";
+}
